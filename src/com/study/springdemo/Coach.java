45bb6c1afd9ea09047eeff2b6f002cc2564bb52d
YifengZhong <yf_zhong@hotmail.com>
2019-11-28 16:26:29 -0600

    add source code

---
+++ src/com/study/springdemo/Coach.java
@@ -0,0 +1,5 @@
+package com.study.springdemo;
+
+public interface Coach {
+	public String getDailyWorkout();
+}
---
+++ src/com/study/springdemo/MyApp.java
@@ -0,0 +1,12 @@
+package com.study.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		//Create Objects
+		Coach theCoach = new TrachCoach();
+		// use object
+		System.out.println(theCoach.getDailyWorkout());
+	}
+
+}
