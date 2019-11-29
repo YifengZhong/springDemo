c7eb6aa47c315dd1d99e73f0f7ccd8cf0271ba48
YifengZhong <yf_zhong@hotmail.com>
2019-11-29 15:39:30 -0600

    Create the bean Container to 1.Load spring configureation file. 2.Retrieve beans from spring container.

--- src/com/study/springdemo/MyApp.java
+++ src/com/study/springdemo/MyApp.java
@@ -4,7 +4,7 @@ public class MyApp {
 
 	public static void main(String[] args) {
 		//Create Objects
-		Coach theCoach = new TrachCoach();
+		Coach theCoach = new TrackCoach();
 		// use object
 		System.out.println(theCoach.getDailyWorkout());
 	}
