5cce21d730372cd7ede752e965ffa608bcf7308c
YifengZhong <yf_zhong@hotmail.com>
2019-11-29 15:42:26 -0600

    Create the bean Container to 1.Load spring configureation file. 2.Retrieve beans from spring container.

---
+++ src/com/study/springdemo/HellpSpringApp.java
@@ -0,0 +1,23 @@
+package com.study.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HellpSpringApp {
+
+	public static void main(String[] args) {
+		//load the spring configureation file
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		//retrieve bean from spring container
+		Coach theCoach = context.getBean("myCoach",Coach.class);
+		
+		//call methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		
+		//close the context
+		context.close();
+
+	}
+
+}
---
+++ src/com/study/springdemo/TrackCoach.java
@@ -0,0 +1,11 @@
+package com.study.springdemo;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Run a hard 5K";
+	}
+
+}
