c28812d369817f617f6025d7e6993a88d27b9a3e
YifengZhong <yf_zhong@hotmail.com>
2019-11-30 11:46:45 -0600

    commit save file

--- src/com/study/springdemo/SetterDemoApp.java
+++ src/com/study/springdemo/SetterDemoApp.java
@@ -13,6 +13,7 @@ public class SetterDemoApp {
 		//call methods on the bean
 		System.out.println(theCoach.getDailyFortune());
 		System.out.println(theCoach.getDailyWorkout());
+		// call our new methods to get the literal values.
 		System.out.println(theCoach.getEmailAddress());
 		System.out.println(theCoach.getTeam());
 		//close the context
