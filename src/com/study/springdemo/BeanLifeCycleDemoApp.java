be05dc68fccbf13bf2dfa37fc7fc2a99ca250e16
YifengZhong <yf_zhong@hotmail.com>
2019-12-01 11:08:35 -0600

    add life cycle methods 1. Define you methods for init and destroy.2. Configure the method names in Spring config file.

---
+++ src/com/study/springdemo/BeanLifeCycleDemoApp.java
@@ -0,0 +1,23 @@
+package com.study.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanLifeCycleDemoApp {
+
+	public static void main(String[] args) {
+		//load the spring configuration file
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		
+		//retrieve bean from spring container
+		Coach theCoach = context.getBean("myCoach",Coach.class);
+		
+		Coach alphaCoach = context.getBean("myCoach",Coach.class);
+	
+		System.out.println("\nMemory location for theCoach:" + theCoach.getDailyWorkout());
+		
+		//close the context
+		context.close();
+	}
+
+}
--- src/com/study/springdemo/TrackCoach.java
+++ src/com/study/springdemo/TrackCoach.java
@@ -19,5 +19,15 @@ public class TrackCoach implements Coach {
 		// TODO Auto-generated method stub
 		return "Just Do It:" + fortuneService.getFortune();
 	}
+	
+	//add an init method
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside method doMystartupStuff");
+	}
+	//add a destroy method
+	//!!!!! For"prototype" scoped beans, Spring does not call the destroy method
+	public void doMyCleanupStuffYoYo() {
+		System.out.println("TrackCoach: inside method doMyCleanupStuffYoYo");
+	}
 
 }
